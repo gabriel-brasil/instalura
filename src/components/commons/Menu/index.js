c993fa05b5ece3040f0e43ce5472ba7b64166326
Gabriel <gabrielbrasil.dev@gmail.com>
2021-03-11 19:59:54 -0300

    Código da Aula 5 / Módulo 1

--- src/components/commons/Menu/index.js
+++ src/components/commons/Menu/index.js
@@ -1,21 +1,22 @@
-import React from "react";
-import { Logo } from "../../../theme/Logo";
-import { Button } from "../Button";
-import { MenuWrapper } from "./styles/MenuWrapper";
+import React from 'react';
+import { Logo } from '../../../theme/Logo';
+import { Text } from '../../foundation/Text';
+import { Button } from '../Button';
+import { MenuWrapper } from './styles/MenuWrapper';
 
 export default function Menu() {
   const links = [
     {
-      texto: "Home",
-      url: "/",
+      texto: 'Home',
+      url: '/',
     },
     {
-      texto: "Perguntas frequentes",
-      url: "/faq",
+      texto: 'Perguntas frequentes',
+      url: '/faq',
     },
     {
-      texto: "Sobre",
-      url: "/sobre",
+      texto: 'Sobre',
+      url: '/sobre',
     },
   ];
 
@@ -28,13 +29,17 @@ export default function Menu() {
         {links.map((link) => {
           return (
             <li key={link.url}>
-              <a href={link.url}>{link.texto}</a>
+              <Text variant="smallestException" tag="a" href={link.url}>
+                {link.texto}
+              </Text>
             </li>
           );
         })}
       </MenuWrapper.CentralSide>
       <MenuWrapper.RightSide>
-        <Button ghost variant="secondary.main">Entrar</Button>
+        <Button ghost variant="secondary.main">
+          Entrar
+        </Button>
         <Button variant="primary.main">Cadastrar</Button>
       </MenuWrapper.RightSide>
     </MenuWrapper>
--- src/theme/index.js
+++ src/theme/index.js
@@ -1,3 +1,5 @@
+import { typographyVariants } from "./typographyVariants";
+
 const colors = {
   background: {
     light: {
@@ -41,7 +43,8 @@ const colors = {
 
 export default {
   colors,
+  typographyVariants,
   fontFamily: '\'Rubik\', sans-serif',
-  borderRadius: '8px',
-  transition: '200ms ease-in-out',
-}; 
+  borderRadius: "8px",
+  transition: "200ms ease-in-out",
+};
