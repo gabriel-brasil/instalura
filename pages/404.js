dc68e26846500264e7dc8e719b3a1094a395ee3a
Gabriel <gabrielbrasil.dev@gmail.com>
2021-03-25 21:07:01 -0300

    Correção page 404

--- pages/404.js
+++ pages/404.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-function DivNotification({ ...children }) {
-  return <div>{children}</div>;
-}
-
 export default function Page404() {
-  return <DivNotification>Página 404! :(</DivNotification>;
+  return (
+    <div>
+      Página 404
+    </div>
+  );
 }
